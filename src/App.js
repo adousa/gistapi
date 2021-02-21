c0820b328695f910dff1b4d70bd817f2b7abf49e
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-02-22 02:53:05 +0400

    import React component to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from "react";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 import Header from "./components/Header";
 import GistList from "./components/GistList";
 import GlobalStyles from "./GlobalStyle";
@@ -12,7 +13,7 @@ const App = () => {
       <GlobalStyles />
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div`
   font-size: 14px;
