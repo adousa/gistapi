5053c81cf666d63f4894eb96c71d46b2e79c6ec4
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-02-22 01:13:06 +0400

    handle empty response

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import Octicon from "react-octicon";
 import Gist from "./Gist";
+import PropTypes from "prop-types";
+
 import { getGistData } from "../state/actions/gistAction";
 
 const GistList = ({ dispatch, gistPublicData }) => {
@@ -10,9 +13,14 @@ const GistList = ({ dispatch, gistPublicData }) => {
   }, [dispatch]);
   return (
     <ListWrapper>
-      {(gistPublicData || []).map((item) => (
-        <Gist key={item.id} {...item} />
-      ))}
+      {gistPublicData.length === 0 && (
+        <EmptyListWrapper>
+          <Octicon name="squirrel" mega />
+          <span>Sorry, we couldn't find any result</span>
+        </EmptyListWrapper>
+      )}
+      {gistPublicData.length !== 0 &&
+        (gistPublicData || []).map((item) => <Gist key={item.id} {...item} />)}
     </ListWrapper>
   );
 };
@@ -26,8 +34,28 @@ const ListWrapper = styled.div`
   margin: auto;
 `;
 
+const EmptyListWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 800px;
+  margin: auto;
+  font-size: 23px;
+  margin-top: 24px;
+  line-height: 66px;
+  color: #b1b1b1;
+`;
+
 const mapStateToProps = (state) => {
   return { gistPublicData: ((state || {}).gistPublicData || []).data };
+};
+
+GistList.propTypes = {
+  dispatch: PropTypes.func,
+  gistPublicData: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  ),
 };
 
 export default connect(mapStateToProps)(GistList);
