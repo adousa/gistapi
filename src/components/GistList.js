24c9f56b45a91ad3fb5db8ac5b004bda95985f97
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-02-21 23:17:13 +0400

    create Gist item and Gist list

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,30 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React, {useEffect, useState} from "react"
+
+import styled from "styled-components";
+import {getPublicGists} from "../services/gistService";
+import Gist from "./Gist";
+
+const GistList = () => {
+    const [gistPublicData, setGistPublicData] = useState([]);
+    useEffect(() => {
+        getPublicGists().then((response) => {
+            const {data} = response;
+            setGistPublicData(data);
+        })
+    }, [])
+
+    return (<ListWrapper>{gistPublicData.map((item) => <Gist t={console.log(item)} key={item.id} {...item} />)}</ListWrapper>)
+}
+
+
+const ListWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    max-width:800px;
+    margin: auto;
+`;
+
 
 export default GistList
