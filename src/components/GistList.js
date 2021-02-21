1a13b79757d1833fa45cfba94ae7968443f2985c
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-02-22 00:51:17 +0400

    use action instead of calling api

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,30 +1,33 @@
-import React, {useEffect, useState} from "react"
-
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
-import {getPublicGists} from "../services/gistService";
 import Gist from "./Gist";
+import { getGistData } from "../state/actions/gistAction";
 
-const GistList = () => {
-    const [gistPublicData, setGistPublicData] = useState([]);
-    useEffect(() => {
-        getPublicGists().then((response) => {
-            const {data} = response;
-            setGistPublicData(data);
-        })
-    }, [])
-
-    return (<ListWrapper>{gistPublicData.map((item) => <Gist key={item.id} {...item} />)}</ListWrapper>)
-}
-
+const GistList = ({ dispatch, gistPublicData }) => {
+  useEffect(() => {
+    dispatch(getGistData());
+  }, [dispatch]);
+  return (
+    <ListWrapper>
+      {(gistPublicData || []).map((item) => (
+        <Gist key={item.id} {...item} />
+      ))}
+    </ListWrapper>
+  );
+};
 
 const ListWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    max-width:800px;
-    margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 800px;
+  margin: auto;
 `;
 
+const mapStateToProps = (state) => {
+  return { gistPublicData: ((state || {}).gistPublicData || []).data };
+};
 
-export default GistList
+export default connect(mapStateToProps)(GistList);
