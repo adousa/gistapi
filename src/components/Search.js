cc3756e64d851531f760665374a809f455eb964e
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-02-22 00:52:26 +0400

    use redux action to search for gist

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import Octicon from 'react-octicon'
+import React from "react";
+import styled from "styled-components";
+import Octicon from "react-octicon";
+import { connect } from "react-redux";
 
-const Search = () => {
+import { searchGistByUsername, getGistData } from "../state/actions/gistAction";
+
+const Search = ({ dispatch }) => {
+  const searchByUsername = (query) => {
+    if (!query || query === "") {
+      dispatch(getGistData());
+    } else {
+      dispatch(searchGistByUsername(query));
+    }
+  };
   return (
     <Wrapper>
       <InputBox>
-      <Octicon name="search" />
-      <Input placeholder="Search Gists for the username"/>
+        <Octicon name="search" />
+        <Input
+          placeholder="Search Gists for the username"
+          onChange={(e) => searchByUsername(e.target.value)}
+        />
       </InputBox>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   padding: 8px;
@@ -33,9 +46,13 @@ const Input = styled.input`
   width: 100%;
   font-size: 16px;
 
-  &:focus{
+  &:focus {
     outline: 0;
   }
 `;
 
-export default Search
+const mapStateToProps = (state) => {
+  return { gistPublicData: ((state || {}).gistPublicData || []).data };
+};
+
+export default connect(mapStateToProps)(Search);
